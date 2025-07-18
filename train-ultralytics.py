90b6b3dcbceb2b133f2a1f6817f638bb8eed786b
dr1nkz <raptorpower1@gmail.com>
2025-07-18 17:59:22 +0300

    validating on test set

--- train-ultralytics.py
+++ train-ultralytics.py
@@ -4,4 +4,5 @@ Yolo = YOLO('yolov9c.pt')
 results = Yolo.train(data='./pigs.yaml', epochs=50, imgsz=640, workers=4)
 
 Yolo = YOLO('/kaggle/working/runs/detect/train/weights/best.pt')
+Yolo.val(data='./pigs.yaml', split='test', imgsz=640)
 Yolo.export(format='onnx', opset=12)
