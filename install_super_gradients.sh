6dad12f64963011a65df255770a29cfabdf85505
dr1nkz <raptorpower1@gmail.com>
2025-09-17 21:46:18 +0300

    versions fix

--- install_super_gradients.sh
+++ install_super_gradients.sh
@@ -3,3 +3,4 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install super-gradients
 pip install torchinfo
+pip install torch==1.13.1 torchvision==0.14.1 --extra-index-url https://download.pytorch.org/whl/cu117
